e5a2cdd83a0e56248fb493246252b00155e4ca97
Tariq-act <khantarikact@gmail.com>
2023-08-04 12:56:18 +0530

    profile page implemetation

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+'use client';
 
-const Profile = () => {
-  return <div>Profile</div>;
+import Profile from '@components/Profile';
+import { useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+
+const MyProfile = () => {
+  const { data: session } = useSession();
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+      setPosts(data);
+    };
+
+    if (session?.user.id) {
+      fetchPosts();
+    }
+  }, [session?.user.id]);
+
+  const handleEdit = () => {};
+  const handleDelete = async () => {};
+
+  return (
+    <Profile
+      name='My'
+      desc='Welcome to your personalized profile page'
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
 };
 
-export default Profile;
+export default MyProfile;
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -43,6 +43,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             }
             width={12}
             height={12}
+            alt='copy-icon'
           />
         </div>
       </div>
