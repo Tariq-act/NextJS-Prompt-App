a47e51a9dc1d2a85867d3e546dbe6eac5eec2841
Tariq <khantarikact@gmail.com>
2023-08-20 12:18:50 +0530

    testing

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import Feed from '@components/Feed';
 
 const Home = () => {
+	console.log(Feed);
   return (
     <section className='w-full flex-center flex-col'>
       <h1 className='head_text text-center'>
